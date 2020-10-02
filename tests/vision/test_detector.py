605724603446a7b71ced113d9d8225cb551a5126
amillert <albertmillert97@gmail.com>
2020-10-02 17:21:32 +0200

    Added tests
    TODO: figure out how to provide argparser arguments for tests to run
    properly

---
+++ tests/vision/test_detector.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+
+from src.vision.detector import Detector
+from src.argparser import args
+
+
+class TestDetector(TestCase):
+    def setUp(self):
+        self.detector = Detector(args)
+
+    def test_generate_paths(self):
+        self.assertEqual(len(self.detector.generate_absolute_paths(["./data/pics"])), 2)
+        self.assertEqual(len(self.detector.generate_absolute_paths([""])), 0)
+        self.assertEqual(len(self.detector.generate_absolute_paths([])), 0)
+        self.assertEqual(len(self.detector.generate_absolute_paths(["./data/pics/cat.jpg"])), 1)
+
+    def test_flatten_generate_paths(self):
+        self.assertEqual(len([*self.detector.flatten(
+            self.detector.generate_absolute_paths(["../data/pics", "../data/pics/cat.jpg"]))]), 3)
+        self.assertEqual(len([*self.detector.flatten(
+            self.detector.generate_absolute_paths([]))]), 0)
+        self.assertEqual(len([*self.detector.flatten(
+            self.detector.generate_absolute_paths([""]))]), 0)
+        self.assertEqual(len([*self.detector.flatten(
+            self.detector.generate_absolute_paths(["../data/pics"]))]), 2)
+        self.assertEqual(len([*self.detector.flatten(
+            self.detector.generate_absolute_paths(["../data/pics/cat.jpg"]))]), 1)
+
+    def test_unique_generate_paths(self):
+        self.assertEqual(len(self.detector.unique(self.detector.flatten(
+            self.detector.generate_absolute_paths(["../data/pics", "../data/pics/cat.jpg"])))), 2)
+        self.assertEqual(len(self.detector.unique(self.detector.flatten(
+            self.detector.generate_absolute_paths(["../data/pics"])))), 2)
+        self.assertEqual(len(self.detector.unique(self.detector.flatten(
+            self.detector.generate_absolute_paths([""])))), 0)
+        self.assertEqual(len(self.detector.unique(self.detector.flatten(
+            self.detector.generate_absolute_paths([])))), 0)
+        self.assertEqual(len(self.detector.unique(self.detector.flatten(
+            self.detector.generate_absolute_paths(["../data/pics/cat.jpg"])))), 1)
