8fec54742b2250d8b91cd09e10856f63a58c7452
amillert <albertmillert97@gmail.com>
2020-10-04 00:14:21 +0200

    Fixed tests regarding the functions' source

--- tests/vision/test_detector.py
+++ tests/vision/test_detector.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from src.helper import functional as fct
+from src.helper import paths_generator as pgen
 from src.argparser import args
 from src.vision.detector import Detector
 
@@ -9,31 +11,26 @@ class TestDetector(TestCase):
         self.detector = Detector(args)
 
     def test_generate_paths(self):
-        self.assertEqual(len(self.detector.generate_absolute_paths(["./data/pics"])), 2)
-        self.assertEqual(len(self.detector.generate_absolute_paths([""])), 0)
-        self.assertEqual(len(self.detector.generate_absolute_paths([])), 0)
-        self.assertEqual(len(self.detector.generate_absolute_paths(["./data/pics/cat.jpg"])), 1)
+        self.assertEqual(len(pgen.generate_absolute_paths(["./data/pics"])), 2)
+        self.assertEqual(len(pgen.generate_absolute_paths([""])), 0)
+        self.assertEqual(len(pgen.generate_absolute_paths([])), 0)
+        self.assertEqual(len(pgen.generate_absolute_paths(["./data/pics/cat.jpg"])), 1)
 
     def test_flatten_generate_paths(self):
-        self.assertEqual(len([*self.detector.flatten(
-            self.detector.generate_absolute_paths(["../data/pics", "../data/pics/cat.jpg"]))]), 3)
-        self.assertEqual(len([*self.detector.flatten(
-            self.detector.generate_absolute_paths([]))]), 0)
-        self.assertEqual(len([*self.detector.flatten(
-            self.detector.generate_absolute_paths([""]))]), 0)
-        self.assertEqual(len([*self.detector.flatten(
-            self.detector.generate_absolute_paths(["../data/pics"]))]), 2)
-        self.assertEqual(len([*self.detector.flatten(
-            self.detector.generate_absolute_paths(["../data/pics/cat.jpg"]))]), 1)
+        self.assertEqual(len([*fct.flatten(pgen.generate_absolute_paths(["../data/pics", "../data/pics/cat.jpg"]))]), 3)
+        self.assertEqual(len([*fct.flatten(pgen.generate_absolute_paths([]))]), 0)
+        self.assertEqual(len([*fct.flatten(pgen.generate_absolute_paths([""]))]), 0)
+        self.assertEqual(len([*fct.flatten(pgen.generate_absolute_paths(["../data/pics"]))]), 2)
+        self.assertEqual(len([*fct.flatten(pgen.generate_absolute_paths(["../data/pics/cat.jpg"]))]), 1)
 
     def test_unique_generate_paths(self):
-        self.assertEqual(len(self.detector.unique(self.detector.flatten(
-            self.detector.generate_absolute_paths(["../data/pics", "../data/pics/cat.jpg"])))), 2)
-        self.assertEqual(len(self.detector.unique(self.detector.flatten(
-            self.detector.generate_absolute_paths(["../data/pics"])))), 2)
-        self.assertEqual(len(self.detector.unique(self.detector.flatten(
-            self.detector.generate_absolute_paths([""])))), 0)
-        self.assertEqual(len(self.detector.unique(self.detector.flatten(
-            self.detector.generate_absolute_paths([])))), 0)
-        self.assertEqual(len(self.detector.unique(self.detector.flatten(
-            self.detector.generate_absolute_paths(["../data/pics/cat.jpg"])))), 1)
+        self.assertEqual(len(fct.unique(fct.flatten(
+            pgen.generate_absolute_paths(["../data/pics", "../data/pics/cat.jpg"])))), 2)
+        self.assertEqual(len(fct.unique(fct.flatten(
+            pgen.generate_absolute_paths(["../data/pics"])))), 2)
+        self.assertEqual(len(fct.unique(fct.flatten(
+            pgen.generate_absolute_paths([""])))), 0)
+        self.assertEqual(len(fct.unique(fct.flatten(
+            pgen.generate_absolute_paths([])))), 0)
+        self.assertEqual(len(fct.unique(fct.flatten(
+            pgen.generate_absolute_paths(["../data/pics/cat.jpg"])))), 1)
