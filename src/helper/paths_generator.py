c3ec1ea685cfb60a9dfb921b8bf83695ed84fab9
amillert <albertmillert97@gmail.com>
2020-10-05 23:32:41 +0200

    Fixed result type of the paths_generator method, since it wasn't really constant.

--- src/helper/paths_generator.py
+++ src/helper/paths_generator.py
@@ -4,7 +4,7 @@ import os
 def generate_absolute_paths(paths):
     abs_paths = [os.path.abspath(path) if not os.path.isabs(path) else path for path in paths]
     return [[os.path.join(abs_path, pic) for pic in os.listdir(abs_path)]
-            if os.path.isdir(abs_path) else abs_path for abs_path in abs_paths]
+            if os.path.isdir(abs_path) else [abs_path] for abs_path in abs_paths]
 
 
 def parent_path(path):
