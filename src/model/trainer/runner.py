3254d33d70359805b6df2541b84329f5d129a9e1
amillert <albertmillert97@gmail.com>
2020-10-08 15:11:28 +0200

    Updated the runner module to work with updated rnn model

--- src/model/trainer/runner.py
+++ src/model/trainer/runner.py
@@ -1,5 +1,5 @@
 from ..preprocessing.custom_dataset import CustomDataset
-from ..seq2seq import Seq2Seq
+from ..rnn_net import RNNNet
 
 import time
 
@@ -19,6 +19,8 @@ class Runner:
         self.track = True
         self.load_x = False
         self.load_y = False
+
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         pass
 
     def learn(self):
@@ -28,10 +30,10 @@ class Runner:
         BATCH_SIZE = 500
 
         assert not DATA_SIZE % BATCH_SIZE, f"BATCH_SIZE must be a divisor of the DATASIZE, " \
-            f"else the model will not have a proper layer size set. Given BATCH_SIZE: {BATCH_SIZE} " \
-            f"and DATA_SIZE: {DATA_SIZE}. " \
-            f"For Your reference, use one of these: " \
-            f"{[x for x in range(DATA_SIZE) if 1 < x <= 1024 and not DATA_SIZE % x]}"
+                                           f"else the model will not have a proper layer size set. Given BATCH_SIZE: {BATCH_SIZE} " \
+                                           f"and DATA_SIZE: {DATA_SIZE}. " \
+                                           f"For Your reference, use one of these: " \
+                                           f"{[x for x in range(DATA_SIZE) if 1 < x <= 1024 and not DATA_SIZE % x]}"
 
         # DIMENSIONS (H_DIMS, F_DIMS, etc.)
 
@@ -40,13 +42,13 @@ class Runner:
         print('Creating batches done')
 
         # TODO: load from the argparser
-        MODEL = Seq2Seq(BATCH_SIZE, 0, 0)
+        MODEL = RNNNet(BATCH_SIZE, 0, 0)
         EPOCHS = 10
         ETA = 5
-        MOMENTUM = 0
+        # MOMENTUM = 0
 
-        CRITERION = nn.MSELoss()
-        OPTIMIZER = torch.optim.SGD(MODEL.parameters(), lr=ETA)  # , momentum=MOMENTUM)
+        CRITERION = nn.CrossEntropyLoss()
+        OPTIMIZER = torch.optim.Adam(MODEL.parameters(), lr=ETA)  # , momentum=MOMENTUM)
         # OPTIMIZER = torch.optim.Adam(MODEL.parameters(), lr=ETA)
 
         NUM_BATCHES = np.ceil(DATA_SIZE / BATCH_SIZE)
@@ -60,15 +62,25 @@ class Runner:
 
             for X, y in BATCHES:
                 batch_count += 1
-                outputs = MODEL(X)
-                naive_loss = CRITERION(outputs, y)
-                print(f"Batch {batch_count} loss: {naive_loss}, mean loss: {loss_total / batch_count}")
-                loss_total += naive_loss.item()
-                predicted = torch.argmax(outputs.reshape(len(outputs), -1).data, 1).float()
-                total_classified += outputs.size(0)
-                correctly_classified += (predicted == outputs).sum().item()
+
+                MODEL.train()
                 OPTIMIZER.zero_grad()
-                naive_loss.backward()
+
+                # move to the GPU if possible
+                # TODO: ensure X and y are Tensors
+                X, y = X.to(self.device), y.to(self.device)
+
+                logits, (state_h, state_c) = MODEL(X)
+                loss = CRITERION(logits.transpose(1, 2), y)
+
+                state_h = state_h.detach()
+                state_c = state_h.detach()
+
+                print(f"Batch {batch_count} loss: {loss}, mean loss: {loss_total / batch_count}")
+                loss_total += loss.item()
+
+                OPTIMIZER.zero_grad()
+                loss.backward()
                 OPTIMIZER.step()
 
                 if not batch_count % 10 and batch_count > 0:
