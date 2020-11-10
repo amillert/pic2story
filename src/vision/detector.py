42cb22f2160da1c9173896a51e106397ba251950
amillert <albertmillert97@gmail.com>
2020-11-10 16:53:16 +0100

    Fixed YOLO object detection

--- src/vision/detector.py
+++ src/vision/detector.py
@@ -14,25 +14,22 @@ class Detector:
 
         self.confidence = args.confidence
         self.paths_img = args.paths_img if args.paths_img else def_pics_paths
-        self.classes = [x.strip() for x in open(args.labels).read().split()]
+        self.classes = [x.strip() for x in open(args.labels).readlines()]
         self.net = cv2.dnn.readNetFromDarknet(args.net_config, args.transfer_weights)
 
     def detect(self):
-        return ((label, confidence)
-                for img_path in fct.flatten(pgen.generate_absolute_paths(self.paths_img))
-                for (label, confidence) in self.predict_label(img_path))
+        # allows duplicates, in case detected in different pictures
+        # TODO: think whether that's desired
+        return (label for img_path in fct.flatten(pgen.generate_absolute_paths(self.paths_img))
+                for label in set(self.predict_label(img_path)))
 
     def predict_label(self, img):
         img = cv2.imread(img)
-
         layers_names = [self.net.getLayerNames()[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
 
-        # TODO: check if blob's size is important
-        # (224, 224),
-        self.net.setInput(cv2.dnn.blobFromImage(img, 1 / 255.0, (416, 416), swapRB=True, crop=False))
+        self.net.setInput(cv2.dnn.blobFromImage(img, 1 / 255.0, (416, 416), (0, 0, 0), True, crop=False))
 
         layerOutputs = self.net.forward(layers_names)
 
-        # TODO: filter top predictions (make sure they regard different object)
-        return [(self.classes[int(np.argmax(scores[5:]))], scores[5:][np.argmax(scores[5:])])
-                for out in layerOutputs for scores in out if scores[5:][np.argmax(scores[5:])] > self.confidence]
+        return [self.classes[int(np.argmax(scores[5:]))] for out in layerOutputs for scores in out
+                if scores[5:][np.argmax(scores[5:])] > self.confidence]
