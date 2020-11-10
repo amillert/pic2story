834a8dc1904205aee958ed78361b9f76f56201a5
amillert <albertmillert97@gmail.com>
2020-11-10 20:09:06 +0100

    Updated main.py, added shuffiling for the predicted labels

--- src/main.py
+++ src/main.py
@@ -4,9 +4,12 @@ from src.argparser import args
 from src.vision.detector import Detector
 from src.model.trainer.runner import Runner
 
+import random
+
 if __name__ == '__main__':
     detected = Detector(args).detect()
+    random.shuffle(detected)
 
     runner = Runner(args)
     MODEL = runner.learn()
-    print(runner.sample(MODEL, 50, ' '.join(set(detected))))
+    print(runner.sample(MODEL, 50, ' '.join(detected)))
