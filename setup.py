c1031a2427951054fc7b8b37666f08fc6fce121b
amillert <albertmillert97@gmail.com>
2020-11-14 23:12:41 +0100

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pic2story',
+    version='',
+    packages=['src', 'src.model', 'src.model.trainer', 'src.model.preprocessing', 'src.corpus', 'src.helper',
+              'src.vision', 'tests', 'tests.helper', 'tests.vision'],
+    url='',
+    license='',
+    author='albertmillert',
+    author_email='',
+    description=''
+)
