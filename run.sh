531b3d6d3c2cf412768dcef9510b592178760008
amillert <albertmillert97@gmail.com>
2020-11-14 21:39:08 +0100

    Updated run.sh script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./main.py -p ./data/
+./src/main.py -p ./data/pics -l ./utils/coco.names -n ./utils/yolo.cfg -t ./utils/yolov3.weights -c 0.8 -b ./data/gutten/morgen/ --ngrams 2 --save_corpus ./cache/corpus_big_500.pkl --read_corpus ./cache/corpus_big_500.pkl --batch_size 64 --epochs 20 --eta 2E-3 -g 3 --hidden 30 --layers 12 --drop_prob 0.3
