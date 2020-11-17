5814fb3e75acce72bde73b8693101cf7545b852b
amillert <albertmillert97@gmail.com>
2020-11-17 14:48:34 +0100

    Approached fixing the corpus building part
    - Upcoming explanations and refactor
    - It's probably not final (can't properly filter out the content; seems
      like there's no reocurring theme in the text-structure of documents)

--- src/corpus/reader.py
+++ src/corpus/reader.py
@@ -6,8 +6,10 @@ import re
 from src.corpus.preprocess.corpus_data import CorpusData
 from src.helper import paths_generator as pgen
 
+END_OF_GUTEN = "\n*End of Project Gutenberg(.|\n)*"
 FROM_TABLE_OF_CONTENTS = "(^.{0,4}[0-9].*\n*)+(.|\n)*"
 AFTER_THE_END = "((THE END)|(The end)|(The End))(.|\n)*"
+PUNCT = '(\s|[,.;:?!])'
 
 
 class Reader:
@@ -32,47 +34,65 @@ class Reader:
                 return pickle.load(rfile)
 
     @staticmethod
-    def generate_windowed_sentences(tokenized, word2idx, window):
+    def generate_windowed_sentences(books, word2idx, window):
         res = []
-        tokens = [word2idx[s] for s in tokenized]
-        for i in range(len(tokens) - window):
-            res.append(tokens[i:i+window])
-
-        print(res[-2:])
+        for book in books:
+            for paragraph in book:
+                tokens = [word2idx[word] for word in paragraph]
+                ile = abs(window - len(tokens))
+                if len(tokens) < window:
+                    res.append(tokens + [word2idx["<PAD>"]] * ile)
+                elif len(tokens) > window:
+                    for i in range(0, ile):
+                        res.append(tokens[i:i + window])
+                else:
+                    res.append(tokens)
         return res
 
     def load_books(self):
         books = []
-         #for book in pgen.generate_absolute_paths(self.paths_books):
         for book in pgen.generate_absolute_paths(self.paths_books)[:self.books_to_read]:
             with open(book, "r") as rfile:
                 # TODO: split on end of sentence punctuation marks
                 #       (dot is not always end of sentence);
                 #       think about punctuation in general
                 try:
-                    text = rfile.read()
-                    # if "THE END" not in text or "Contents" not in text:
-                    #     continue
-                    # else:
-                    books.append(re.sub(
-                        "\[([A-Za-z0-9.,;]| )+\]", "", re.sub(
-                            AFTER_THE_END, "", re.sub(
-                                FROM_TABLE_OF_CONTENTS, "", ' '.join(text.split()), 1))
-                            .replace("-\n", "")
+                    # TODO: let's take into consideration only a bit longer paragraphs
+                    #       and run windowing inside
+
+                    """
+                      1. read file
+                      2. remove front / end parts
+                      3. replace 've, 's, etc.
+                      4. find long enough paragraphs, by splitting through chars and find ones with over thresh
+                    """
+
+                    text = (
+                        re.sub(AFTER_THE_END, "", re.sub(
+                            FROM_TABLE_OF_CONTENTS, "", re.sub(
+                                END_OF_GUTEN, "", rfile.read())))
+                            .replace("-\n", "")  # continuation of the line
+                            .replace("-", "")
                             .replace("'t", " not")
+                            .replace("'re", " are")
                             .replace("'d", " would")
                             .replace("'s", " is")
-                            .replace("'ve", " have")))
+                            .replace("'ve", " have"))
+                    tmp = [re.split(PUNCT, x) for x in text.split("\n\n")]  # split on punctuation
+                    tmp = [[xi for xi in x if (xi.isalpha() and len(xi) > 1) or (not xi.isalpha() and len(xi) == 1)] for x in tmp]
+                    tmp = [[xi.strip() for xi in x if xi.strip()] for x in tmp if len(x) > 6]  # remove paragraphs with too few components; leaving us with paragraphs
+                    books.append(tmp[20:])  # make sure we begin with text; even tho it's not beginning
                 except:
                     pass
 
-        return [xi.strip() for x in re.split('(\s"|[,.;:?!"-]\s)', ' '.join(books)) for xi in x.split()]
+        return books  # list of lists of strings (tokens)
 
     def preprocess(self):
-        tokenized_books = self.load_books()
+        books = self.load_books()
+        naive_tokens = [token for book in books for tokens in book for token in tokens]
         print("tokenization done")
 
-        word2freq = Counter(tokenized_books)
+        word2freq = Counter(naive_tokens)
         print("word2freq done")
         vocabulary = ["<PAD>"] + list(word2freq.keys())
         print("vocab done")
@@ -83,9 +103,12 @@ class Reader:
         print("index done")
 
         windowed_sentences = self.generate_windowed_sentences(
-            tokenized_books, word2idx, self.window
+            books, word2idx, self.window
         )
         print("windowing done")
+        with open("/home/devmood/PycharmProjects/pic2story/cache/windows", "w") as f:
+            for w in windowed_sentences:
+                f.write(f"{str(w)}\n")
 
         return windowed_sentences, vocabulary, word2idx, idx2word
 
